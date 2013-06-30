e3c03232e30b7fa8ad87fda19ed01118b14a576e
Konstantin Martini <k@tuxcode.org>
2013-06-30 07:54:24 +0200

    added more fileendings to media parser

--- src/catchers/media.sh
+++ src/catchers/media.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep -io 'http://[^ ]*\.\(mp3\|mp4\|avi\|mpg\|mpeg\|wmv\|ogg\|m4a\|m4v\|jpg\|png\|bmp\|wav\|flac\|pdf\|txt\)' "$1"
+grep -io 'http://[^ ]*\.\(mp3\|mp4\|avi\|mpg\|mpeg\|wmv\|ogg\|m4a\|m4v\|jpg\|png\|bmp\|wav\|flac\|pdf\|txt\|svg\|aac\|gif\|swf\|rtf\|ps\|zip\|gz\|bz2\|rar\|tar\|jpeg\|mov\|divx\)' "$1"
